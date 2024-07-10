e1c5d186ccc1f851d6ba73301294b6938a8284ae
nshin09 <153220860+nshin09@users.noreply.github.com>
2024-07-10 12:42:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const scenes = [
                 d.AverageHighwayMPG = +d.AverageHighwayMPG;
             });
 
-            // Sort data by AverageHighwayMPG in ascending order
-            data.sort((a, b) => d3.ascending(a.AverageHighwayMPG, b.AverageHighwayMPG));
+            // Sort data by AverageHighwayMPG in descending order
+            data.sort((a, b) => d3.descending(a.AverageHighwayMPG, b.AverageHighwayMPG));
 
             const svg = d3.select("#visualization").append("svg")
                 .attr("width", "100%")
@@ -39,7 +39,7 @@ const scenes = [
                 .data(data)
                 .enter().append("rect")
                 .attr("class", "bar")
-                .style("font-size", "12px") // Adjust font size here
+                .style("font-size", "10px") // Adjust font size here
                 .attr("x", 0)
                 .attr("y", d => y(d.Make + ' ' + d.Fuel))
                 .attr("width", d => x(d.AverageHighwayMPG))
@@ -112,8 +112,8 @@ const scenes = [
                 .padding(0.1);
 
             function updateChart(metric) {
-                // Sort data by selected metric in ascending order
-                data.sort((a, b) => d3.ascending(a[metric], b[metric]));
+                // Sort data by selected metric in descending order
+                data.sort((a, b) => d3.descending(a[metric], b[metric]));
 
                 x.domain([0, d3.max(data, d => d[metric])]);
                 y.domain(data.map(d => d.Make + ' ' + d.Fuel));
@@ -222,7 +222,7 @@ const scenes = [
             function updateChart() {
                 // Filter and sort data by selected metric and fuel types
                 const filteredData = data.filter(d => selectedFuelTypes.includes(d.Fuel));
-                filteredData.sort((a, b) => d3.ascending(a[selectedMetric], b[selectedMetric]));
+                filteredData.sort((a, b) => d3.descending(a[selectedMetric], b[selectedMetric]));
 
                 x.domain([0, d3.max(filteredData, d => d[selectedMetric])]);
                 y.domain(filteredData.map(d => d.Make + ' ' + d.Fuel));
